614ecac26f79e1a8c5935e6a231bb1f2d6a7bdc6
Yracerlyn <yessicerlyn@gmail.com>
2024-05-18 13:21:35 +0200

    first page

---
+++ front/src/components/navLog.vue
@@ -0,0 +1,32 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            <img :src="logo" alt="logo" height="30">
+          </a>
+          
+            <form class="d-flex">
+                <ul class="navbar-nav ml-auto"> 
+                    <li class="nav-item"> 
+                        <a href="#" class="nav-link">Login</a> 
+                    </li> 
+                    <li class="nav-item"> 
+                        <a href="#" class="nav-link">Sign up</a> 
+                    </li> 
+                </ul> 
+            </form>
+        </div>
+      </nav>
+</template>
+
+<script>
+    export default {
+        name : 'navLog',
+        data() {
+            return {
+                logo: require('@/assets/logo_efrei.png')
+            }
+        }
+    }
+</script>
+
